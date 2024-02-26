02ffa7f16241e3ee5eada6a75e3dfbdbef058e7a
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2024-02-26 20:45:27 -0300

    Update baseUrl and esModuleInterop configuration

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,5 +1,1 @@
-// export const baseUrl =
-//   import.meta.env.VITE_API_URL || 'https://is-it-safe-api-v2.herokuapp.com';
-
-export const baseUrl =
-  process.env.VITE_API_URL || 'https://is-it-safe-api-v2.herokuapp.com';
+export const baseUrl = process.env.VITE_API_URL as string;
