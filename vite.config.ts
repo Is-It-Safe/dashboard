777eec18e1de1570a5c093cd97fa4adc25fff1b2
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2024-02-26 21:12:12 -0300

    Update environment variables and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import EnvironmentPlugin from 'vite-plugin-environment';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), EnvironmentPlugin('all')],
+  plugins: [react(), svgr()],
   esbuild: {
     target: 'esnext',
   },
